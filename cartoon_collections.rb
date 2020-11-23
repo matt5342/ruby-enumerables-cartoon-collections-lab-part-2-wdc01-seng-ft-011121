6767619497b853bde80d5940826728522217f9ba
Matthew Sewell <mattfsewell@gmail.com>
2020-11-23 23:20:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
   
-  arrya.map { |num| num*num }
+  array.map { |num| num*num }
   
 end
 
